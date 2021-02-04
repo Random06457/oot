6fe0569c09b8306898c58617a0cc90f900d52ba8
Random <28494085+Random06457@users.noreply.github.com>
2021-02-04 20:00:04 +0100

    remove /scenes

--- nonmatchings/func_800AC2F4/compile.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-INPUT="$(readlink -f "$1")"
-OUTPUT="$(readlink -f "$3")"
-cd /home/random/n64/decomp/oot
-tools/ido_recomp/linux/7.1/cc -c -G 0 -non_shared -Xfullwarn -Xcpluscomm -Iinclude -Isrc -Iassets -Ibuild -Wab,-r4300_mul -woff 649,838,712 -mips2 -O2 "$INPUT" -o "$OUTPUT"
